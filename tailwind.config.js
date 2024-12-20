2a5c1a8afb1c5d4914136ada46fd1c521807bf61
guppuboss <internetdreamz@gmail.com>
2024-12-20 11:23:24 +0500

    tailwind file reverse

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
-/*eslint-env node*/
-/** @type {import('tailwindcss').Config} */
-
-export default {
+module.exports = {
   content: ['index.html', './src/**/*.{ts,tsx}'],
   safelist: [
     'demo1',
